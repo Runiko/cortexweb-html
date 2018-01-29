a42af9a4b9d9272f7abcc7e2e84f8b4a866d77ca
Runiko <daniel.zdziebko.00@gmail.com>
2018-01-29 12:57:26 +0100

    Change to PHP schema, step 1

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+include 'views/app.php'
+?>
+
+<div class="row">
+  <ol class="breadcrumb">
+    <li><a href="index.php"><?php echo $Strings['Homepage']; ?></a></li>
+    <li class="active"><?php echo $Strings['AboutApp']; ?></li>
+  </ol>
+</div>
+
+<?php PageEnd(); ?>
